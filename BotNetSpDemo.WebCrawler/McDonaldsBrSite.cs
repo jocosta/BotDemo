1776af43ea9694c462dc23bfb60e13eef22a6493
652977 <652977@SPOQSNOTINO7901>
2017-09-18 18:20:13 -0300

    Add project files.

---
+++ BotNetSpDemo.WebCrawler/McDonaldsBrSite.cs
@@ -0,0 +1,56 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+
+namespace BotNetSpDemo.WebCrawler
+{
+    public class McDonaldsBrSite
+    {
+        private const string SITE_URL = @"http://www.mcdonalds.com.br";
+
+        public IEnumerable<ItemMenu> GetMenu()
+        {
+
+
+            return null;
+        }
+
+        public List<ItemMenu> GetMeatMenu()
+        {
+            List<ItemMenu> list = new List<ItemMenu>();
+
+            string Url = $"{SITE_URL}/cardapio/sanduiches-de-carne";
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(Url);
+            //var nodes = doc.DocumentNode.ChildNodes[2].ChildNodes[3].SelectNodes("//div[@id=\"list-details-v1\"]/div/div");
+
+           var nodes = doc.DocumentNode.SelectNodes("//main/div[2]/div/div");
+
+            foreach (var node in nodes)
+            {
+
+                var item = node.SelectSingleNode("a");
+
+                var tagImg = item.SelectSingleNode("div[1]/div[1]/img[1]").Attributes["src"].Value;
+                var tagName = item.SelectSingleNode("div[2]").InnerText;
+
+                list.Add(new ItemMenu
+                {
+                    SandwicheName = tagName,
+                    ImgUrl = tagImg
+                });
+
+            }
+
+            return list;
+        }
+    }
+
+
+    [Serializable]
+    public class ItemMenu
+    {
+        public string SandwicheName { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
---
+++ BotNetSpDemo/PedidoForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.FormFlow.Advanced;
+using BotNetSpDemo.WebCrawler;
+using System.Threading.Tasks;
+
+namespace BotNetSpDemo
+{
+    public enum Sandwiches
+    {
+
+    }
+
+    public enum Drinks
+    {
+
+    }
+
+    public enum PaymentType
+    {
+
+    }
+
+    [Serializable]
+    public class PedidoForm
+    {
+        public string Name { get; set; }
+        public string Sandwiche { get; set; }
+        public Drinks Drink { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public bool IsVoyage { get; set; }
+
+        public static IForm<PedidoForm> BuildForm()
+        {
+
+            var builder = new FormBuilder<PedidoForm>()
+                .Message("Bem-vindo ao bot do MC! \U0001F609")
+                .Field(new FieldReflector<PedidoForm>(nameof(Sandwiche))
+                            .SetType(null)
+                            .SetDefine((state, field) =>
+                            {
+                                var crawler = new McDonaldsBrSite();
+                                var dados = crawler.GetMeatMenu().Distinct();
+
+                                foreach (var prod in dados)
+                                    field
+                                        .SetAllowsMultiple(true)
+                                        .SetPrompt(new PromptAttribute("Selecione o seu sanduiche {||}"))
+                                        .AddDescription(prod, prod.SandwicheName, prod.ImgUrl)
+                                        .AddTerms(prod, prod.SandwicheName);
+
+                                return Task.FromResult(true);
+                            }))
+                 .AddRemainingFields();
+              
+
+            builder.Confirm("Legal, finalizamos o preenchimento do seu cadastro. Pra finalizar, confirme os dados abaixo: {*} {||}");
+            //builder.OnCompletion(async (context, cadastro) =>
+            //{
+
+
+
+            //                 //activity.Text = $"{cadastro.NomeCompleto}, seu cadastro foi realizado com sucesso. Em instantes chegará no email  {cadastro.Email} um novo e-mail, fique ligado!";
+            //    await context.PostAsync(null);
+            //});   //IMessageActivity activity = context.MakeMessage();
+
+
+            var form = builder.Build();
+
+
+            return form;
+        }
+
+
+    }
+}
